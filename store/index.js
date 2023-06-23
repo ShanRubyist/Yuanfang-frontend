b9322978f9b35463aed6855d24678df6cb04a59d
shan <2302531309@qq.com>
2023-06-23 10:52:11 +0800

    FEAT: 使用 cookie-universal-nuxt 持久化 cookies

--- store/index.js
+++ store/index.js
@@ -1,13 +1,25 @@
 export const state = () => ({
-    token: null,
-    userid: null
-  })
-  
-  export const mutations = {
-    set_token (state, token) {
-      state.token = token
-    },
-    set_userid (state, userid) {
-      state.userid = userid
+  token: null,
+  userid: null
+})
+
+export const mutations = {
+  set_token(state, token) {
+    state.token = token
+    this.$cookies.set('token', token)
+  },
+  set_userid(state, userid) {
+    state.userid = userid
+  }
+}
+
+// 初始化数据方法
+export const actions = {
+  async nuxtServerInit({ commit }, { app }) {
+    let token = app.$cookies.get('token')
+
+    if (token) {
+      commit('set_token', token)
     }
   }
+}
