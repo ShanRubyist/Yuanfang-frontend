f7c68a69e04ce528e97e02c18a9af76bdee3a72f
shan <2302531309@qq.com>
2023-06-23 15:01:57 +0800

    REFACTOR: 部分代码放到 utils 目录下

--- pages/users/_id/index.vue
+++ pages/users/_id/index.vue
@@ -23,7 +23,7 @@
           <div class="showcase_header">{{ label }}</div>
 
           <div class="showcase_body" @click="copy(result.find(item => item.label === label)?.answer)">
-            <div v-html="markdown(result.find(item => item.label === label)?.answer)"></div>
+            <div v-html="md(result.find(item => item.label === label)?.answer)"></div>
           </div>
 
         </div>
@@ -35,6 +35,8 @@
   
 <script>
 import request from '@/utils/request'
+import { parseSSEMessage, copyToClipboard } from '@/utils/lib'
+import markdown from '@/utils/markdown'
 
 const MAPPER_MODEL_LIST = [
   { 'label': 'gpt-3.5-turbo(openai)', 'model': 'gpt-3.5-turbo', 'site': 'openai' },
@@ -103,7 +105,7 @@ export default {
 
           for (let i = 0; i < messages.length - 1; i++) {
             const message = messages[i]
-            const { field, value } = this.parseSSEMessage(message)
+            const { field, value } = parseSSEMessage(message)
             // console.log('field', field)
             // console.log('value', value)
 
@@ -115,115 +117,16 @@ export default {
       }
       catch (e) {
         this.result.find(item => item.label === label).answer += ("\n\nOpenAI 请求:" + e)
-
       }
       finally {
         this.loading = false
       }
     },
-    parseSSEMessage(message) {
-      const lines = message.split('\n')
-      const lineFields = []
-      const lineValues = []
-      for (const line of lines) {
-        const { field, value } = this.parseSSELine(line)
-        lineFields.push(field)
-        lineValues.push(value)
-      }
-      if (new Set(lineFields).size > 1) {
-        throw new Error(
-          `Multiple fields appear in a message: ${new Set(lineFields).values()}`,
-        )
-      }
-      return { field: lineFields[0], value: lineValues.join('\n') }
-    },
-    parseSSELine(line) {
-      const pos = line.indexOf(': ')
-      if (pos === -1)
-        throw new Error(`Can't find ': ' in line '${line}'`)
-      if (pos === 0)
-        return { field: '', value: line.slice(2) }
-      return {
-        field: line.slice(0, pos),
-        value: line.slice(pos + 2),
-      }
-    },
-    markdown(str) {
-      if (typeof str === 'undefined') return '';
-
-      const md = require('markdown-it')({
-        html: false,            // Enable HTML tags in source
-        xhtmlOut: false,        // Use '/' to close single tags (<br />).
-        // This is only for full CommonMark compatibility.
-        breaks: false,        // Convert '\n' in paragraphs into <br>
-        langPrefix: 'language-',  // CSS language prefix for fenced blocks. Can be
-        // useful for external highlighters.
-        linkify: false,        // Autoconvert URL-like text to links
-
-        // Enable some language-neutral replacement + quotes beautification
-        // For the full list of replacements, see https://github.com/markdown-it/markdown-it/blob/master/lib/rules_core/replacements.js
-        typographer: false,
-
-        // Double + single quotes replacement pairs, when typographer enabled,
-        // and smartquotes on. Could be either a String or an Array.
-        //
-        // For example, you can use '«»„“' for Russian, '„“‚‘' for German,
-        // and ['«\xA0', '\xA0»', '‹\xA0', '\xA0›'] for French (including nbsp).
-        quotes: '“”‘’',
-
-        // Highlighter function. Should return escaped HTML,
-        // or '' if the source string is not changed and should be escaped externally.
-        // If result starts with <pre... internal wrapper is skipped.
-        highlight: function (/*str, lang*/) { return ''; }
-      });
-      return md.render(str)
-    },
-    async copyToClipboard(text) {
-      if (!text) return
-      try {
-        return await navigator.clipboard.writeText(text)
-      } catch {
-        const element = document.createElement("textarea")
-        const previouslyFocusedElement = document.activeElement
-
-        element.value = text
-
-        // Prevent labelboard from showing on mobile
-        element.setAttribute("readonly", "")
-
-        element.style.contain = "strict"
-        element.style.position = "absolute"
-        element.style.left = "-9999px"
-        element.style.fontSize = "12pt" // Prevent zooming on iOS
-
-        const selection = document.getSelection()
-        const originalRange = selection
-          ? selection.rangeCount > 0 && selection.getRangeAt(0)
-          : null
-
-        document.body.appendChild(element)
-        element.select()
-
-        // Explicit selection workaround for iOS
-        element.selectionStart = 0
-        element.selectionEnd = text.length
-
-        document.execCommand("copy")
-        document.body.removeChild(element)
-
-        if (originalRange) {
-          selection?.removeAllRanges() // originalRange can't be truthy when selection is falsy
-          selection?.addRange(originalRange)
-        }
-
-        // Get the focus back on the previously focused element, if any
-        if (previouslyFocusedElement) {
-          ; (previouslyFocusedElement).focus()
-        }
-      }
+    md(str) {
+      return markdown(str)
     },
     async copy(content) {
-      await this.copyToClipboard(content)
+      await copyToClipboard(content)
     }
   },
 }
---
+++ utils/lib.js
@@ -0,0 +1,73 @@
+export function parseSSEMessage(message) {
+    const lines = message.split('\n')
+    const lineFields = []
+    const lineValues = []
+    for (const line of lines) {
+        const { field, value } = parseSSELine(line)
+        lineFields.push(field)
+        lineValues.push(value)
+    }
+    if (new Set(lineFields).size > 1) {
+        throw new Error(
+            `Multiple fields appear in a message: ${new Set(lineFields).values()}`,
+        )
+    }
+    return { field: lineFields[0], value: lineValues.join('\n') }
+}
+
+export function parseSSELine(line) {
+    const pos = line.indexOf(': ')
+    if (pos === -1)
+        throw new Error(`Can't find ': ' in line '${line}'`)
+    if (pos === 0)
+        return { field: '', value: line.slice(2) }
+    return {
+        field: line.slice(0, pos),
+        value: line.slice(pos + 2),
+    }
+}
+
+export async function copyToClipboard(text) {
+    if (!text) return
+    try {
+        return await navigator.clipboard.writeText(text)
+    } catch {
+        const element = document.createElement("textarea")
+        const previouslyFocusedElement = document.activeElement
+
+        element.value = text
+
+        // Prevent labelboard from showing on mobile
+        element.setAttribute("readonly", "")
+
+        element.style.contain = "strict"
+        element.style.position = "absolute"
+        element.style.left = "-9999px"
+        element.style.fontSize = "12pt" // Prevent zooming on iOS
+
+        const selection = document.getSelection()
+        const originalRange = selection
+            ? selection.rangeCount > 0 && selection.getRangeAt(0)
+            : null
+
+        document.body.appendChild(element)
+        element.select()
+
+        // Explicit selection workaround for iOS
+        element.selectionStart = 0
+        element.selectionEnd = text.length
+
+        document.execCommand("copy")
+        document.body.removeChild(element)
+
+        if (originalRange) {
+            selection?.removeAllRanges() // originalRange can't be truthy when selection is falsy
+            selection?.addRange(originalRange)
+        }
+
+        // Get the focus back on the previously focused element, if any
+        if (previouslyFocusedElement) {
+            ; (previouslyFocusedElement).focus()
+        }
+    }
+}
